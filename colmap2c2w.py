b96cc7333a72f8175b7a7abca1eb7c5be7ead25f
Zhensheng Yuan <564361+yzslab@users.noreply.github.com>
2022-12-06 00:23:09 +0800

    Update colmap2c2w.py parameter name

--- colmap2c2w.py
+++ colmap2c2w.py
@@ -2,9 +2,9 @@ import argparse
 import internal.colmap.colmap
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", required=True, type=str)
+parser.add_argument("--model-dir", required=True, type=str)
 parser.add_argument("--out-dir", required=True, type=str)
 parser.add_argument("--out-name", default="transforms.npy")
 args = parser.parse_args()
 
-internal.colmap.colmap.colmap2c2w(args.model, args.out_dir, args.out_name)
+internal.colmap.colmap.colmap2c2w(args.model_dir, args.out_dir, args.out_name)
